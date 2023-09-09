defca3b3b830f4eb92fc1fa028fa623866e600e1
Bruno Kim <brunokim.mc@gmail.com>
2023-09-09 01:27:34 -0300

    Completando explicação do interpretador naive

--- pyrinha/main.py
+++ pyrinha/main.py
@@ -222,12 +222,8 @@ class BinaryOp(Enum):
     GT = Operator(">", 20)
     LTE = Operator("<=", 20)
     GTE = Operator(">=", 20)
-    AND = Operator("&", 10)
-    OR = Operator("|", 5)
-
-    # TODO: NOT não é uma operação binária
-    # https://github.com/aripiprazole/rinha-de-compiler/issues/10
-    NOT = Operator("!", 25)
+    AND = Operator("&&", 10)
+    OR = Operator("||", 5)
 
 
 @frozen
@@ -301,6 +297,14 @@ class Str(Term):
         return repr(self.value)
 
 
+@frozen
+class Bool(Term):
+    value: bool
+
+    def __str__(self):
+        return "true" if self.value else "false"
+
+
 """
 Estas variáveis visam criar uma relação entre (nome da class Term) -> classe.
 Isto é útil para podermos converter um objeto serializado da AST, que possui
@@ -311,7 +315,7 @@ de Term, o que garantiria que uma nova classe Term sempre estará presente, mas
 preferi fazer desse jeito repetitivo e simples.
 """
 
-term_classes = [Let, Function, If, Binary, Call, Print, Var, Int, Str]
+term_classes = [Let, Function, If, Binary, Call, Print, Var, Int, Str, Bool]
 term_by_kind = {cls.__name__.lower(): cls for cls in term_classes}
 
 # ---- Read AST ----
@@ -416,15 +420,6 @@ class Env:
     # confiar apenas na nossa disciplina.
     values: dict[str, Value] = field(factory=dict, converter=dict)
 
-    @classmethod
-    def global_env(cls):
-        return Env(
-            {
-                "true": Literal(True),
-                "false": Literal(False),
-            }
-        )
-
     def with_values(self, extra: dict[str, Value]) -> Value:
         "Cria um novo Env com base no atual, e contendo as associações em extra"
         values = dict(self.values)
@@ -441,8 +436,8 @@ Em runtime, uma função captura o environment onde ela foi declarada, para ser
 pura e imutável. Considere por exemplo:
 
     let x = 1;                   // Env: {x: 1}
-    let f = fn (a) { a + x };    // Env: {x: 1, f: <Closure#... fn (a)>}
-    let x = 2;                   // Env: {x: 2, f: <Closure#... fn (a)>}
+    let f = fn (a) { a + x };    // Env: {x: 1, f: <#closure>}
+    let x = 2;                   // Env: {x: 2, f: <#closure>}
     print(f(10))
 
 Ao chamar a função na linha 4, esperamos que imprima '11', tendo capturado o
@@ -456,8 +451,7 @@ class Closure(Value):
     env: Env
 
     def __str__(self):
-        ptr = hex(id(self))[-6:]
-        return f"<Closure#{ptr} fn ({args})>"
+        return "<#closure>"
 
 
 # ---- Interpreter ----
@@ -471,60 +465,131 @@ interpretador se baseia somente em _calcular valores_, computando uma saída
 a partir das entradas.
 
 Vamos escrever primeiro uma função 'evaluate0' que é bem simples e
-ineficiente, mas que pelo menos vai servir de padrão para desenvolvimentos
-mais complexos posteriores. Essa função será chamada recursivamente quando for
-necessário calcular sub-valores.
+ineficiente, mas que pelo menos vai servir de padrão para compararmos e validarmos
+desenvolvimentos mais complexos posteriores.
 
-É interessante que quase todas as expressões podem produzir
+Essa função é invocada para um dado 'Term', e por sua vez ela pode ser invocada
+novamente para 'Term's filhos do primeiro. Por exemplo, para calcular o valor de
+'10 + x*20', invocamos
 
-Uma das facilidades
-é não existir a possibilidade de uma variável mudar de valor, o que complicaria
-um pouco a implementação de Closure. Podemos apenas referenciar o environment
-presente onde uma função é declarada, que todos os valores serão os mesmos em
-todos os instantes onde ela será executada.
+    evaluate0(Binary(Int(10), ADD, Binary(Var("x"), MUL, Int(20))))
+
+Internamente para calcular o valor de Binary(..., ADD, ...), precisamos calcular
+antes
+
+    evaluate0(Int(10))
+    evaluate0(Binary(Var("x"), MUL, Int(20)))
+
+Por sua vez, este último ainda precisa calcular
+
+    evaluate0(Var("x"))
+    evaluate0(Int(20))
+
+Perceba que essas chamadas recursivas ocorrem para cada nó da árvore de sintaxe
+abstrata, o que caracteriza esse interpretador como "tree-walking". Como discutido
+anteriormente, não é indicado realizar chamadas recursivas sem ter um limite de
+profundidade conhecido, então vamos melhorar isso mais pra frente.
 """
 
 
+@frozen
 class ExecutionError(Exception):
-    pass
+    """Exceção lançada para erros de runtime.
+
+    Poderia se chamar RuntimeError, mas esse já é um tipo de Python.
+
+    Note que podemos usar o mesmo decorator '@frozen' para tipos próprios
+    de exceções.
+    """
+
+    location: Loc
+    msg: str
 
 
-def run_file(file: File):
-    return evaluate(Env.global_env(), file.expression)
+def run_file0(file: File) -> Value:
+    "Executa um arquivo no environment global."
+
+    """
+    No momento o environment global contém apenas algumas poucas definições.
+    Numa linguagem completa, você iria querer incluir toda a biblioteca
+    padrão.
+    """
+    return evaluate0(Env(), file.expression)
 
 
-def evaluate(env: Env, term: Term) -> tuple[Env, Value]:
+def evaluate0(env: Env, term: Term) -> Value:
+    "Obtém o valor de um termo para um dado environment."
     match term:
-        case Int(_, value):
-            return env, Literal(value)
-        case Str(_, value):
-            return env, Literal(value)
-        case Var(_, text):
+        # O caso de Int, Str e Bool são os mais simples, retornando um Literal com o
+        # mesmo valor presente na AST.
+        case Int(value=value):
+            return Literal(value)
+        case Str(value=value):
+            return Literal(value)
+        case Bool(value=value):
+            return Literal(value)
+
+        # Para obter o valor de Var, consultamos se a variável está definida
+        # no environment. Ela pode ter sido definida por um Let anterior, ou como
+        # o parâmetro de uma função.
+        case Var(location, text):
             if text not in env.values:
-                raise ExecutionError(f"unknown variable '{text}'")
-            return env, env.values[text]
-        case Let(_, name, value, next):
-            _, val = evaluate(env, value)
-            next_env = env.with_values({name.text: val})
-            if isinstance(val, Closure):
-                # Let of fn is recursive, so closure must be able to reference itself.
-                new_val = evolve(val, env=next_env)
-                next_env.values[name.text] = new_val
-            return evaluate(next_env, next)
+                raise ExecutionError(location, f"unknown variable '{text}'")
+            return env.values[text]
+
+        # Encontrar uma função anônima não exige nenhuma ação a não ser capturar
+        # o environment onde ela é definida para criar sua Closure.
         case Function():
-            return env, Closure(term, env)
-        case If(_, condition, then, otherwise):
-            _, cond = evaluate(env, condition)
+            return Closure(term, env)
+
+        # Para obter o valor de um 'if', precisamos apenas obter o valor de um dos
+        # seus branches -- ou 'then', ou 'otherwise'.
+        case If(location, condition, then, otherwise):
+            cond = evaluate0(env, condition)
             match cond:
                 case Literal(True):
-                    return evaluate(env, then)
+                    return evaluate0(env, then)
                 case Literal(False):
-                    return evaluate(env, otherwise)
+                    return evaluate0(env, otherwise)
                 case _:
-                    raise ExecutionError(f"condition in 'if' is not boolean")
-        case Binary(_, lhs, op, rhs):
-            _, lhs = evaluate(env, lhs)
-            _, rhs = evaluate(env, rhs)
+                    cond_type = type(cond).__name__
+                    raise ExecutionError(
+                        location, f"condition in 'if' is {cond_type}, not bool"
+                    )
+
+        # Optei que a operação Print retorne o valor passado para ela, além de imprimi-lo pra stdout.
+        # O objetivo é poder debugar uma função como
+        #
+        #     let x = foo(a, b);
+        #
+        # inserindo prints em cada elemento:
+        #
+        #     let x = print(foo(print(a), print(b)))
+        #
+        # TODO: adicionar isso no spec da linguagem.
+        case Print(value=value):
+            val = evaluate0(env, value)
+            print(val)
+            return val
+
+        # O cálculo de operações binárias é bem direto, mas longo e repetitivo.
+        # É possível fazer algo mais sucinto para obter a operação a partir do token, por
+        # exemplo, criando um mapa
+        #
+        # import operator
+        #
+        # operation_by_token = {"+": operator.add, "-": operator.sub, ...}
+        #
+        # Ainda assim, eu prefiro ser mais explícito que sucinto. Demora mais pra escrever
+        # mas muito menos pra ler, e dar manutenção nisso é muito mais fácil do que se tivesse
+        # uma ou duas camadinhas extras de abstração!
+        #
+        # Também cabe dizer que eu não tenho medo de usar 's/regex/repl/g' no meu editor de texto
+        # preferido (Vim), para fazer find-and-replace espertos em várias linhas! Recomendo
+        # fortemente que você pratique isso no seu editor de preferência.
+        case Binary(location, lhs, op, rhs):
+            lhs = evaluate0(env, lhs)
+            rhs = evaluate0(env, rhs)
             match lhs, rhs:
                 case Literal(), Literal():
                     pass
@@ -533,83 +598,139 @@ def evaluate(env: Env, term: Term) -> tuple[Env, Value]:
                         f"Invalid operands for '{op.value.token}': {lhs}, {rhs}"
                     )
 
+            # A saber: eu amo o match-case introduzido no Python 3.10. Ele possui ainda outras
+            # capacidades sintáticas para não dar vontade nunca mais de fazer um instanceof().
+            # Leia o tutorial completo em https://peps.python.org/pep-0636/
             match op.value.token, lhs.x, rhs.x:
                 case "+", int(), int():
-                    return env, Literal(lhs.x + rhs.x)
+                    return Literal(lhs.x + rhs.x)
+                case "+", str(), int():
+                    return Literal(lhs.x + str(rhs.x))
+                case "+", int(), str():
+                    return Literal(str(lhs.x) + rhs.x)
                 case "+", str(), str():
-                    return env, Literal(lhs.x + rhs.x)
+                    return Literal(lhs.x + rhs.x)
                 case "-", int(), int():
-                    return env, Literal(lhs.x - rhs.x)
+                    return Literal(lhs.x - rhs.x)
                 case "*", int(), int():
-                    return env, Literal(lhs.x * rhs.x)
+                    return Literal(lhs.x * rhs.x)
                 case "/", int(), int():
-                    return env, Literal(lhs.x // rhs.x)
+                    return Literal(lhs.x // rhs.x)
                 case "%", int(), int():
-                    return env, Literal(lhs.x % rhs.x)
+                    return Literal(lhs.x % rhs.x)
+                # TODO: verificar se precisamos lançar um erro para == e != caso sejam de tipos diferentes.
                 case "==", _, _:
-                    return env, Literal(lhs == rhs)
+                    return Literal(lhs == rhs)
                 case "!=", _, _:
-                    return env, Literal(lhs != rhs)
+                    return Literal(lhs != rhs)
+                # TODO: verificar se podemos usar os operadores de comparação também com strings.
                 case "<", int(), int():
-                    return env, Literal(lhs.x < rhs.x)
-                case "<", str(), str():
-                    return env, Literal(lhs.x < rhs.x)
+                    return Literal(lhs.x < rhs.x)
                 case ">", int(), int():
-                    return env, Literal(lhs.x > rhs.x)
-                case ">", str(), str():
-                    return env, Literal(lhs.x > rhs.x)
+                    return Literal(lhs.x > rhs.x)
                 case "<=", int(), int():
-                    return env, Literal(lhs.x <= rhs.x)
-                case "<=", str(), str():
-                    return env, Literal(lhs.x <= rhs.x)
+                    return Literal(lhs.x <= rhs.x)
                 case ">=", int(), int():
-                    return env, Literal(lhs.x >= rhs.x)
-                case ">=", str(), str():
-                    return env, Literal(lhs.x >= rhs.x)
-                case "&", bool(), bool():
-                    return env, Literal(lhs.x and rhs.x)
-                case "|", bool(), bool():
-                    return env, Literal(lhs.x or rhs.x)
+                    return Literal(lhs.x >= rhs.x)
+                case "&&", bool(), bool():
+                    return Literal(lhs.x and rhs.x)
+                case "||", bool(), bool():
+                    return Literal(lhs.x or rhs.x)
                 case _:
                     raise ExecutionError(
-                        f"Invalid operands for '{op.value.token}': {lhs}, {rhs}"
+                        location,
+                        f"Invalid operands for '{op.value.token}': {lhs}, {rhs}",
                     )
-        case Call(_, callee, arguments):
-            _, f = evaluate(env, callee)
+
+        # A função principal do Let não é criar um novo valor, mas sim um novo environment.
+        # Nós avaliamos 'value' e associamos ao nome 'name' em um novo environment,
+        # construído sobre o anterior.
+        #
+        # A maior diferença está no tratamento do Let de uma função anônima. Para outras
+        # variáveis, não faz sentido permitir que uma variável se refira a si mesmo:
+        #
+        #     let x = f(x + 1);  // Se não existir um x definido anteriormente, isso gera um erro.
+        #
+        # Mas para funções, queremos poder chamá-las recursivamente (ainda mais em uma linguagem
+        # sem loops):
+        #
+        #     let pot = fn(a, b) {     // pot(a, b) calcula a^b.
+        #       let step = fn(i, x) {  // invariante: x = a^i
+        #         if i == b { x }
+        #         else      { step(i + 1, x * a) }
+        #       };
+        #       step(0, 1)
+        #     }
+        #
+        # Perceba que 'fn(i, x)' é avaliada no escopo de 'pot', e portanto captura as variáveis 'a' e 'b',
+        # que são referenciadas no seu corpo. Contudo, ela também referencia 'step', que _não estaria_ no
+        # environment no momento da sua definição. Tratamos as Closures, então, como um caso especial.
+        #
+        # Note que, com isso, não conseguimos referenciar uma função anterior no corpo de outra com o mesmo
+        # nome.
+        case Let(name=name, value=value, next=next):
+            val = evaluate0(env, value)
+            next_env = env.with_values({name.text: val})
+            if isinstance(val, Closure):
+                # Cria nova Closure contendo a definição de name.
+                new_val = Closure(val.function, next_env)
+                # Modifica o dicionário do env para usar a nova Closure.
+                next_env.values[name.text] = new_val
+            return evaluate0(next_env, next)
+
+        # Por fim, implementamos chamadas, ou aplicações, de função.
+        #
+        # Nós pareamos os parâmetros declarados da função com os argumentos passados.
+        # Com isso, construímos um novo environment com base no environment capturado da closure.
+        # Pos fim, basta avaliar a função com esse novo environment.
+        case Call(location, callee, arguments):
+            f = evaluate0(env, callee)
             if not isinstance(f, Closure):
-                raise ExecutionError(f"'{f}' is not callable")
+                raise ExecutionError(location, f"'{f}' is not callable")
             if len(arguments) != len(f.function.parameters):
                 raise ExecutionError(
+                    location,
                     f"'{f.function}' called with {len(arguments)} arguments"
+                    f" (expecting {len(f.function.parameters)})",
                 )
-            values = (evaluate(env, arg) for arg in arguments)
+            values = (evaluate0(env, arg) for arg in arguments)
             params = {
-                param.text: value
-                for param, (_, value) in zip(f.function.parameters, values)
+                param.text: value for param, value in zip(f.function.parameters, values)
             }
             call_env = f.env.with_values(params)
-            return evaluate(call_env, f.function.value)
-        case Print(_, value):
-            _, val = evaluate(env, value)
-            print(val, end="")
-            return env, val
+            return evaluate0(call_env, f.function.value)
+
         case _:
-            raise ExecutionError(f"Unexpected type {type(term)}")
+            raise ExecutionError(
+                term.location, f"Unexpected type {type(term).__name__}"
+            )
 
 
 # ---- Main ----
 
 
-def main(ast_obj):
+def main(ast_obj: dict):
+    # Aqui finalmente usamos o converter que construímos para os Node da árvore, contendo
+    # alguns hooks customizados de desserialização.
     node = converter.structure(ast_obj, File)
     print(node)
     print()
-    run_file(node)
+
+    # Executa o arquivo. Note que não estou imprimindo o resultado de run_file, para
+    # não confundir com um possível print de dentro da linguagem.
+    run_file0(node)
 
 
 if __name__ == "__main__":
     from argparse import ArgumentParser
     from pathlib import Path
+
+    """
+    Eu tenho por hábito sempre usar argparse.ArgumentParser, mesmo que pudesse só ler
+    o parâmetro direto de 'sys.argv'. Não custa quase nada, você tem um --help pra se
+    lembrar como seu script funciona, e invariavelmente eles só vão acumulando mais
+    opções, que ficam difíceis de gerenciar sem um parser de flags de linha de comando.
+    """
 
     p = ArgumentParser()
     p.add_argument("file", type=Path, help="AST file to execute")
